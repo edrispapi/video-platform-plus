286bae07374208a66e3f913c45c16f0623cda1b9
ادریس پاپی <64495523+edrispapi@users.noreply.github.com>
2025-07-20 02:01:53 +0330

    Update models.py

--- users/models.py
+++ users/models.py
@@ -2,9 +2,13 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
-    # سایر فیلدها
+    pass
 
 class Profile(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="profiles")
     name = models.CharField(max_length=64)
     preferences = models.JSONField(default=dict, blank=True)
+
+class DashboardConfig(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    widgets = models.JSONField(default=dict, blank=True)
